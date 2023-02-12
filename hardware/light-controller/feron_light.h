cd0f38714322e904c7b4bf3044f657c68e3e17ec
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2023-02-12 21:00:25 +0400

    Changes

--- hardware/light-controller/feron_light.h
+++ hardware/light-controller/feron_light.h
@@ -1,13 +1,16 @@
 #include "esphome.h"
 #include "esphome/components/light/light_output.h"
+#include "esphome/components/switch/switch.h"
 #include "esphome/components/remote_base/remote_base.h"
 #include "esphome/components/remote_base/nec_protocol.h"
 
 using namespace esphome;
 using namespace esphome::light;
 using namespace esphome::remote_base;
+using namespace esphome::switch_;
 
 #define FERON_ADDRESS_DIM_BH  0xB721
+#define FERON_ADDRESS_SET_BH  0xB781
 #define FERON_ADDRESS_NATIVE  0xB7A0
 #define FERON_POWER_OFF       0xEC
 
@@ -33,7 +36,7 @@ class FeronLightOutput : public LightOutput {
     uint16_t address = FERON_ADDRESS_NATIVE, command = FERON_POWER_OFF;
     state->current_values_as_ct(&color, &brightness);
     if (brightness > 0.0f) {
-      address = FERON_ADDRESS_DIM_BH;
+      address = this->fade_on() ? FERON_ADDRESS_DIM_BH : FERON_ADDRESS_SET_BH;
       command = static_cast<uint16_t>(0.5f + brightness * 15.0f) * 16 + static_cast<uint16_t>(15.5f - color * 15.0f);
     }
     ESP_LOGD("feron_light", "Set color: %f, brightness: %f. NEC command: 0x%02X", color, brightness, command);
@@ -50,7 +53,10 @@ class FeronLightOutput : public LightOutput {
     transmit(remote, {FERON_ADDRESS_NATIVE, static_cast<uint16_t>(preset + 32)});
   }
   void set_remote_transmitter(RemoteTransmitterBase *remote) { this->remote_ = remote; }
+  void set_fade_switch(Switch *sw) { this->fade_ = sw; }
+  bool fade_on() const { return (this->fade_ == nullptr) || this->fade_->state; }
 
  protected:
-  RemoteTransmitterBase *remote_;
+  RemoteTransmitterBase *remote_{};
+  Switch *fade_{};
 };
